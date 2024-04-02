9fc6e0fe4e5c6799111b67cdea280712f9544a41
mesuli95 <makelenimesuli@gmail.com>
2024-04-02 03:01:02 -0700

    code

---
+++ Week 3/dd.gui.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from tkinter import ttk
+
+class DailyDigestGUI:
+    
+    def _init_(self, root):
+        pass # build the GUI
+    
+    """
+    The GUI should enable the admin to ...
+      - configure which content sources to inclued in email
+      - add recipients
+      - remove recipients
+      - schedule daily time to send email
+      - configure sender credentials   
+    """
+    
+if __name__ =='_main_':
+        root = Tk()
+        app =DailyDigestGUI(root)
+        root.mainloop()
+        
+        
+    
---
+++ Week 3/dd_email.py
@@ -0,0 +1,13 @@
+class DailyDigestEmail:
+    
+    def _init_(self):
+        pass
+    
+    def send_email(self):
+        pass
+    
+    def format_message(self):
+        pass
+    
+    if __name__== '_main_':
+        pass # test code
